d53c25a0ce700286f671dc00719e4c6ebac872e1
Vsoftcrackers <vsoftsolutions8813@gmail.com>
2024-12-26 13:20:52 +0530

    Final i think

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -88,7 +88,7 @@ const Header = () => {
     <header className="header">
       <div className="logo-container">
       
-        <img src="assets/spinner.png" alt="Logo" className="logo" style={{ width: "120px", height:"40px"}} />
+        <img src="assets/spinner.png" alt="Logo" className="logo" style={{ width: "160px", height:"50px"}} />
       </div>
 
     
